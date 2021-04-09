c8c34e97a23541c103928950cf72bd17b88e42ce
anilson <anilsonmonteiro@gmail.com>
2021-04-09 22:08:25 -0100

    add camera

--- car.js
+++ car.js
@@ -10,3 +10,19 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
 directionalLight.position.set(200, 500, 300);
 scene.add(directionalLight);
 
+// setting up the camera
+const aspectRatio = window.innerWidth / window.innerHeight;
+const cameraWidth = 150;
+const cameraHeight = cameraWidth/aspectRatio;
+
+const camera = new THREE.OrthographicCamera(
+    cameraWidth / -2, // left
+    cameraWidth / 2, // right
+    cameraHeight / 2, // top
+    cameraHeight / -2, // bottom
+    0, // near plane
+    1000 // far plane
+);
+
+camera.position.set(200, 200, 200);
+camera.lookAt(0, 10, 0);
